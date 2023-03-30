58a9775ee918d5a6590e05348c9ae82e757e2a3f
Yajassardana <yajassardana@gmail.com>
2023-03-30 18:22:03 +0530

    fix web3auth whitelist domains issue redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -173,7 +173,7 @@ const login = async() => {
         rpcUrl: "https://wiser-alien-morning.matic-testnet.discover.quiknode.pro/c2f6cfc05517853e094ad7ea47188326625f20b5/"
       },
       openLogin: {
-        redirectUrl: `http://localhost:3000/`,
+        redirectUrl: [window.location.origin],
       },
     };
     const gaslessOnboarding = new GaslessOnboarding(
